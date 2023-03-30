d15463f5a6d22eb4a13f8c0446694d1bfc3c08eb
marcosvillarreal <marcosevillarreal@gmail.com>
2023-03-30 19:16:28 -0300

    piva

--- proyectos/starossa/Scripts/PM_Lote_Facturas_Naty.sql
+++ proyectos/starossa/Scripts/PM_Lote_Facturas_Naty.sql
@@ -1,7 +1,7 @@
 use datos
 go
 select --vendedor.nombre, maopera.numcomp,maopera.idvendedor,
-cabefac.*--,pedipda.* ,
+cabefac.idmaopera,vendedor.nombre,cabefac.cnombre,cabefac.total,maopera.numcomp--,pedipda.* ,
 --tablaimp.*
 from maopera 
 left join vendedor on maopera.idvendedor = vendedor.id
@@ -11,5 +11,10 @@ inner join pedipda on cabefac.idmaopera = pedipda.idmaopera
 where terminal= 8 and maopera.origen='FAC'
 --maopera.origen='FPE'
 and maopera.id between 2890846 and 2890873
---and cabefac.id in (2461782,2461781)
+--and cabefac.idmaopera in (2890868)
 order by maopera.id desc --, tablaimp.id
+--select * from cuerfac where idmaopera = 2890868
+
+--go
+--select * from cabeunifica where id = 1000837
+--select * from cabepromo where idprodgenera in (1004870,1004143,1003663)
