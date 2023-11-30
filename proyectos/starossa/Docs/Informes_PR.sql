6121b0ce6789da8ed59e0ff15c86a220f94be45c
marcosvillarreal <marcosevillarreal@gmail.com>
2023-11-30 20:58:35 -0300

    datos

--- proyectos/starossa/Docs/Informes_PR.sql
+++ proyectos/starossa/Docs/Informes_PR.sql
@@ -19,7 +19,7 @@ go
 --go
 
 --select distinct convert(char(10),ca.fecha,105) as fecha,c.cnumero as codigo,c.cnombre as razonsocial,pr.nombre as provincia, l.nombre  as localidad ,c.cdireccion as direccion
---,cv.nombre as canalvta,v.nombre as vendedor,mar.nombre as marca,p.codalfa,p.codbarra13 as ean,p.nombre as producto,cu.cantidad,cu.preunitasiva,round(cu.cantidad*cu.preunitasiva,3)
+--,cv.nombre as canalvta,mar.nombre as marca,p.codalfa,p.codbarra13 as ean,p.nombre as producto,cu.cantidad,cu.preunitasiva,round(cu.cantidad*cu.preunitasiva,3)
 --from maopera  as m
 --inner join cabefac as ca on m.id = ca.idmaopera
 --inner join cuerfac as cu on ca.idmaopera = cu.idmaopera
@@ -45,7 +45,7 @@ go
 ------Padron de todos los clientes que compran alcochol
 
 --select distinct c.cnumero as codigo,c.cnombre as razonsocial,pr.nombre as provincia, l.nombre  as localidad ,c.cdireccion as direccion
---,cv.nombre as canalvta,'ACTIVO' as estado
+--,cv.nombre as canalvta,'ACTIVO' as estado,v.nombre as vendedor
 --from maopera  as m
 --inner join cabefac as ca on m.id = ca.idmaopera
 --inner join cuerfac as cu on ca.idmaopera = cu.idmaopera
@@ -55,14 +55,16 @@ go
 --inner join localidad as l on c.idlocalidad = l.id
 --inner join canalvta as cv on c.idcanalvta = cv.id
 --inner join rubro as r on p.idrubro = r.id
+--left join vendedor as v on m.idvendedor = v.id
 --where m.estado = '0' and m.clasecomp in ('A','B','C','H','X')
 --and ca.fecha between '20230101' and '20230831'
 --and p.espromocion = 0
 --and r.idtipoprod = 2
+--and (not v.id  in (5,8,9) and v.prevta=1 and v.oficina=0)
 
----------------Clientes cepas
+---------------Clientes por proveedor
 select distinct c.cnumero as codigo,c.cnombre as razonsocial,pr.nombre as provincia, l.nombre  as localidad ,c.cdireccion as direccion
-,cv.nombre as canalvta,'ACTIVO' as estado
+,cv.nombre as canalvta,'ACTIVO' as estado,v.nombre as vendedor
 from maopera  as m
 inner join cabefac as ca on m.id = ca.idmaopera
 inner join cuerfac as cu on ca.idmaopera = cu.idmaopera
@@ -71,6 +73,16 @@ inner join ctacte as c on ca.idctacte = c.id
 inner join provincia as pr on c.idprovincia = pr.id
 inner join localidad as l on c.idlocalidad = l.id
 inner join canalvta as cv on c.idcanalvta = cv.id
+left join vendedor as v on m.idvendedor = v.id
 where m.estado = '0' and m.clasecomp in ('A','B','C','H','X')
 and ca.fecha between '20230101' and '20231031'
-and p.idctacte = 7102 and p.espromocion = 0
+and p.idctacte in (7097) and p.espromocion = 0
+--and c.cnumero = '895'
+and (not v.id  in (5,8,9) and v.prevta=1 and v.oficina=0)
+
+--select * from ctacte where cnombre like '%campari%'
+--7102	10095 	FRATELLI BRANCA DESTILERIAS SA (BENJAMIN
+--7097	10071 	CEPAS ARGENTINAS                        
+--7106	10102 	CAMPARI ARGENTINA S.A. (SANTIAGO CABAK) 
+
+select * from vendedor where prevta=1 and oficina=0
